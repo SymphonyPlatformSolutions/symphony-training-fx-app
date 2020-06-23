6dfd11b4d0c1f377a86c4808fc4a51e3318110ee
Yong Sheng Tan <yong.tan@symphony.com>
2020-06-23 15:10:46 +0800

    8.6 Building the FxWatchlist Component

--- extension-app/pages/fx-watchlist/index.js
+++ extension-app/pages/fx-watchlist/index.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    Text, Card, Box, Button, DecisionDropdown
+} from 'symphony-bdk-ui-toolkit';
 
-const FxWatchlist = () => <>Hello World</>;
+const FxWatchlist = () => {
+    const [ newCurrency, setNewCurrency ] = useState();
+    const [ watchlistData, setWatchlistData ] = useState([]);
+
+    const addToList = () => {
+        const { base, value: currency } = newCurrency;
+        if (!watchlistData.some(i => i.base === base && i.currency === currency)) {
+            setWatchlistData(previous => [ ...previous, { base, currency } ]);
+        }
+        setNewCurrency();
+    };
+
+    const fxRefData = [{
+        suboptions: [
+            {
+                label: 'USD Base',
+                options: [
+                    { label: 'USD/CAD', value: 'CAD', base: 'USD' },
+                    { label: 'USD/JPY', value: 'JPY', base: 'USD' }
+                ]
+            },
+            {
+                label: 'EUR Base',
+                options: [
+                    { label: 'EUR/CHF', value: 'CHF', base: 'EUR' },
+                    { label: 'EUR/GBP', value: 'GBP', base: 'EUR' }
+                ]
+            }
+        ]
+    }];
+
+    return (
+        <React.Fragment>
+            <Text size="large" type="primary">Your Watchlist</Text>
+            <Card>
+                <b>Insert New Currency</b>
+                <Box horizontal>
+                    <DecisionDropdown onChange={setNewCurrency} value={newCurrency} data={fxRefData} />
+                    <Button size="large" onClick={addToList}>Add to List</Button>
+                </Box>
+            </Card>
+            { watchlistData.map((item, index) => <Box key={index}>{item.base}/{item.currency}</Box>) }
+        </React.Fragment>
+    );
+};
 
 export default FxWatchlist;
